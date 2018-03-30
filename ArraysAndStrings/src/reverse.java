df93ce841215ec40fd3b66b29bbf751e46b109e5
Melvin Tham <melvintham1997@gmail.com>
2018-03-29 21:05:06 -0400

    Reverse String

---
+++ ArraysAndStrings/src/reverse.java
@@ -0,0 +1,17 @@
+public class reverse {
+
+    public static void main(String[] args)
+    {
+        String test = "hello";
+        reverseString(test);
+    }
+
+    static void reverseString(String str){
+        for (int i = str.length()-1; i>=0; i--)
+        {
+            System.out.print(str.charAt(i));
+        }
+    }
+}
+
+
