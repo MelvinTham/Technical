0cf3cefc85bd5ec3281d59ac13fe10163b19ebc1
MelvinTham <melvintham1997@gmail.com>
2018-04-13 00:14:15 -0400

    Added Node Class

---
+++ LinkedList/src/LinkedList.java
@@ -0,0 +1,18 @@
+
+public class LinkedList {
+
+    class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start of Linked List");
+    }
+}
